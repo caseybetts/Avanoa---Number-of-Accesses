31adc43db99aa1ae3f5866de7ab4c5f1bab95ad4
Casey Betts <caseyandsusana@gmail.com>
2024-10-14 10:00:15 -0600

    First commit

---
+++ Avanoa_Script_Test.py
@@ -0,0 +1,11 @@
+# Author: Casey Betts, 2024
+# This script outputs data on how many accesses the given orders have on a given day
+
+
+# Steps:
+# Run Tersedia for each spacecraft
+# Create a list of order ids for each spacecraft
+# Create a set of all unique order ids
+# Create a columnn in a the output shapefile for number of accesses
+    # Column calculation is the number of lists the order id is found in
+# Output shapefile 
