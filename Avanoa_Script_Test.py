4679df8858fc8e96431fa62755c689c68cee9a06
Casey Betts <caseyandsusana@gmail.com>
2024-11-14 11:22:27 -0700

    Moves locations to a config file

--- Avanoa_Script_Test.py
+++ Avanoa_Script_Test.py
@@ -1,11 +1,13 @@
 # Author: Casey Betts, 2024
 # This script outputs data on how many accesses the given orders have on a given day
-# Requirements: all onv layers and active ufp layer in the table of contents with the correct names
+# Requirements: all onv layers and active ufp layer in the table of contents (does not require specific layer names)
 
 import arcpy
-from functools import reduce
+import json
 import os
 import shutil
+
+from functools import reduce
 
 def get_layer_by_name(layer_name, map):
     """
@@ -24,7 +26,12 @@ def get_layer_by_name(layer_name, map):
 
 # Select the available orders on a given layer
 def select_available_orders(orders_layer, onv_layer, scid):
-    """ Select orders accessable on a given rev based on the order's max ONA vlaue """
+    """ Select orders accessable on a given rev based on the order's max ONA vlaue 
+    
+    :param orders_layer: Feature Layer, layer of orders to select from
+    :param onv_layer: Feature Layer, layer of spacecraft onv to intersect the orders layer 
+    :param scid: String, the spacecraft id that the selected orders must be active on
+    """
 
     arcpy.AddMessage(f"Running available_orders for {scid}.....")
 
@@ -51,36 +58,22 @@ def select_available_orders(orders_layer, onv_layer, scid):
 
     arcpy.AddMessage("Done")
 
-def create_available_orders_featuer_class(scid, m):
-    """ Create a feature class of all available orders on a given spacecraft and return the list of order ids"""
-
-    # get the onv and orders layers
-    onv_layer = get_layer_by_name('onv_'+scid+'_Avanoa', m)
-    orders_layer = get_layer_by_name('active_orders_ufp_Avanoa', m)
-
-    # Select the available orders on the orders layer
-    select_available_orders(orders_layer, onv_layer, scid)
-
-    # Export the selection to the defalut GDB
-    temp_layer = arcpy.env.workspace + "\\" + 'available_' + scid + '_temp'
-    arcpy.conversion.ExportFeatures(orders_layer, temp_layer)
-
-    # Create a list of order ids
-    with arcpy.da.SearchCursor(temp_layer, ["external_id"]) as cursor:
-        active_orders = [row[0] for row in cursor]
-
-    arcpy.AddMessage(scid + " :" + str(active_orders[:10]))
-
-    return active_orders
-
 def find_layer_by_source(map, source_path, query_req):
-    """ Returns a layer of the given source and name"""
+    """ Returns a layer of the given source and name
 
-    # Loop through layers looking for matching source and base
+    :param map: Map Object, the map containing the layer
+    :param source_path: String, Url to the geoserver location
+    :param query_req: String, an SQL query expression
+    """
+
+    # Loop through layers looking for matching URL and Name
     for layer in map.listLayers():
         try:
             desc = arcpy.Describe(layer)
             query = layer.listDefinitionQueries()[0]
+
+            # Return the layer if Url matches the given path and the query requirement is in the query
+            # (the onv layers will have a query specifying the day)
             if desc.catalogPath == source_path and query_req in query['sql']:
                 return layer
 
@@ -89,15 +82,12 @@ def find_layer_by_source(map, source_path, query_req):
 
     return None
 
-def change_def_query(layer, query):
-    """ Change the definition query of the given layer to the given string """
-
-    layer.definitionQuery = query
-
-
-
 def get_selected(layer, field):
-    """ Returns a list of order ids from the given layer that are currently selected """
+    """ Returns a list of order ids from the given layer that are currently selected 
+    
+    :param layer: Feature Layer, the layer with the selection
+    :param field: String, the field from which to gather the values from selected rows
+    """
 
     desc = arcpy.Describe(layer)
     oid_field = desc.OIDFieldName 
@@ -117,6 +107,8 @@ def get_selected(layer, field):
 def delete_files(folder):
     """ 
     Deletes out the files in the given folder
+
+    :param folder: String, the path to the folder of files to delete 
     """
 
     for file in os.listdir(folder):
@@ -131,7 +123,10 @@ def delete_files(folder):
 
 def move_files(source_folder, target_folder):
     """ 
-    Moves all files in a given folder to the given output folder    
+    Moves all files in a given folder to the given output folder  
+
+    :param souce_folder: String, the path of the folder to move files from
+    "param target_folder: String, the path of the folder to move files to  
     """
 
     for file in os.listdir(source_folder):
@@ -140,18 +135,27 @@ def move_files(source_folder, target_folder):
         shutil.move(source_path, dest_path)
 
 def run_workflow(path, days):
-    """ This function calls all functions in the needed order to produce final output """
+    """ This function calls all functions in the needed order to produce final output 
+    
+    :param path: String, the path to the folder containing this tool
+    :param days: Integer, the number of days in the future to assess accesses on each order
+    """
 
-    scids = ['wv02']#, 'wv02', 'wv03', 'ge01', 'lg01', 'lg02', 'lg03', 'lg04']
-    staging_loc = os.path.join(path, "Staging")
-    output_loc = os.path.join(path, "Output")
-    output_feature = os.path.join(staging_loc, "Avanoa_test")
+    # Load .json file with parameters
+    with open('config.json', 'r', errors="ignore") as file:
+        configs = json.load(file)
+
+    scids = configs["scids"]
+    staging_loc = os.path.join(path, configs["staging_name"])
+    output_loc = os.path.join(path, configs["output_name"])
+    output_feature = os.path.join(staging_loc, configs["output_feature"])
     
     # Get the active map document and data frame
     m = arcpy.mp.ArcGISProject("CURRENT").activeMap
 
     # Get the orders layer
-    orders_layer = get_layer_by_name('active_orders_ufp_Avanoa', m)
+    orders_source = configs["orders_layer_source"] + "\\" + configs["orders_layer_name"]
+    orders_layer = find_layer_by_source(m, orders_source, "")
 
     # Create lists of available orders for each spacecraft and add the order lists to a dictionary
     order_lists = dict()
@@ -159,7 +163,7 @@ def run_workflow(path, days):
 
         for day in range(1, days+1):
 
-            onv_source = f"https://geoserver-gds-prod-rehearsal.app.maxaranalytics.com/geoserver/ows?\onv_{scid}"
+            onv_source = configs["onv_layer_source"] + "\\" + f"onv_{scid}"
 
             # get the onv layer
             onv_layer = find_layer_by_source(m, onv_source, f'days = {day}')
